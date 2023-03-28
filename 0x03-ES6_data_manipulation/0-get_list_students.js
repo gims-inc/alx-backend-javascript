90a97f11687fc4fa008e6e5773827602082decbf
gims-inc <gabrielmusanga@gmail.com>
2023-03-28 16:39:42 +0300

    Basic list of objects --eslint fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,22 +1,20 @@
 /*
- id (Number), firstName (String), and location (String). 
+ id (Number), firstName (String), and location (String).
     Guillaume, id: 1, in San Francisco
     James, id: 2, in Columbia
     Serena, id: 5, in San Francisco
 */
 
-export default function getListStudents(){
+export default function getListStudents() {
+  const studentList = [];
 
-    let studentList = [];
+  const studentOne = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const studentTwo = { id: 2, firstName: 'James', location: 'Columbia' };
+  const studentThree = { id: 5, firstName: 'Serena', location: 'San Francisco' };
 
-    const studentOne =  {id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-    const studentTwo =  { id: 2, firstName: 'James', location: 'Columbia' };
-    const studentThree = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  studentList.push(studentOne);
+  studentList.push(studentTwo);
+  studentList.push(studentThree);
 
-    studentList.push(studentOne);
-    studentList.push(studentTwo);
-    studentList.push(studentThree);
-
-
-    return ([...studentList]);
+  return ([...studentList]);
 }
