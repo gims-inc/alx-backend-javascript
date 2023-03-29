a9b776d1cf66aaa926bb3a9ff39f133684368cab
gims-inc <gabrielmusanga@gmail.com>
2023-03-29 08:42:13 +0300

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((param) => (param !== undefined ? param.startsWith(startString) : ''))
+    .map((param) => (param !== undefined ? param.slice(startString.length) : ''))
+    .join('-');
+}
