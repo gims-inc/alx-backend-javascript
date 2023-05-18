bdd00ea964aed207f0d6bb419ff03460b8ea76de
gims-inc <gabrielmusanga@gmail.com>
2023-05-19 01:20:32 +0300

    spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable jest/valid-expect */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable jest/prefer-expect-assertions */
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi using SUM in calculateNumber method of Utils', () => {
+    const fxnspy = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(fxnspy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(fxnspy.calculateNumber.callCount).to.be.equal(1);
+    fxnspy.calculateNumber.restore();
+  });
+});
